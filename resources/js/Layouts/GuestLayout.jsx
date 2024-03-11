d440da63b8b6953f38b4dddf27d29ee4f9aaf683
muhasyraf <faizkamil39@gmail.com>
2024-03-11 21:42:58 +0700

    fix(auth): classname typo

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -6,9 +6,9 @@ export default function Guest({ children }) {
         <div className="min-h-screen flex items-center guest-background md:bg-white">
             <div className="flex-1 mx-auto">
                 <div className="max-md:px-4 md:flex md:flex-row">
-                    <div class="h-auto md:w-1/2 hidden md:block">
+                    <div className="h-auto md:w-1/2 hidden md:block">
                         <img
-                            class="object-cover w-full h-screen"
+                            className="object-cover w-full h-screen"
                             src="https://images.unsplash.com/photo-1624281450963-e33ac96170be?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                             alt="img"
                         />
