69bc45f9d98f925f38271f94cf9cd59d8a9b437f
muhasyraf <faizkamil39@gmail.com>
2024-03-11 12:08:43 +0700

    feat(auth): authentication page design slicing

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Checkbox from "@/Components/Checkbox";
 import GuestLayout from "@/Layouts/GuestLayout";
 import InputError from "@/Components/InputError";
@@ -13,6 +13,7 @@ export default function Login({ status, canResetPassword }) {
         password: "",
         remember: false,
     });
+    const [visiblePassword, setVisiblePassword] = useState(false);
 
     useEffect(() => {
         return () => {
@@ -26,6 +27,10 @@ export default function Login({ status, canResetPassword }) {
         post(route("login"));
     };
 
+    const togglePasswordVisibility = () => {
+        setVisiblePassword(!visiblePassword);
+    };
+
     return (
         <GuestLayout>
             <Head title="Log in" />
@@ -37,7 +42,19 @@ export default function Login({ status, canResetPassword }) {
             )}
 
             <form onSubmit={submit}>
-                <div>
+                <h1 className="text-5xl font-bold tracking-tight text-gray-900">
+                    Sign In
+                </h1>
+                <div className="flex gap-1 items-center mt-4">
+                    <span className="text-sm text-gray-600">New user?</span>
+                    <Link
+                        href={route("register")}
+                        className="text-sm text-sky-600 hover:text-sky-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    >
+                        Create an account
+                    </Link>
+                </div>
+                <div className="mt-4">
                     <InputLabel
                         htmlFor="email_or_username"
                         value="Email or Username"
@@ -54,24 +71,77 @@ export default function Login({ status, canResetPassword }) {
                         onChange={(e) =>
                             setData("email_or_username", e.target.value)
                         }
+                        required
                     />
 
                     <InputError message={errors.email} className="mt-2" />
                     <InputError message={errors.username} className="mt-2" />
                 </div>
 
-                <div className="mt-4">
-                    <InputLabel htmlFor="password" value="Password" />
+                <div className="relative mt-4">
+                    <div className="flex justify-between items-center">
+                        <InputLabel htmlFor="password" value="Password" />
+                        {canResetPassword && (
+                            <Link
+                                href={route("password.request")}
+                                className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                            >
+                                Forgot?
+                            </Link>
+                        )}
+                    </div>
 
                     <TextInput
                         id="password"
-                        type="password"
+                        type={visiblePassword ? "text" : "password"}
                         name="password"
                         value={data.password}
                         className="mt-1 block w-full"
                         autoComplete="current-password"
                         onChange={(e) => setData("password", e.target.value)}
+                        required
                     />
+                    <div
+                        className="absolute inset-y-[3rem] right-0 flex items-center px-4 text-gray-600"
+                        onClick={togglePasswordVisibility}
+                    >
+                        {!visiblePassword ? (
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth={1.5}
+                                stroke="currentColor"
+                                className="w-6 h-6 cursor-pointer"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
+                                />
+                            </svg>
+                        ) : (
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth={1.5}
+                                stroke="currentColor"
+                                className="w-6 h-6 cursor-pointer"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+                                />
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                                />
+                            </svg>
+                        )}
+                    </div>
 
                     <InputError message={errors.password} className="mt-2" />
                 </div>
@@ -91,22 +161,11 @@ export default function Login({ status, canResetPassword }) {
                     </label>
                 </div>
 
-                <div className="flex items-center justify-end mt-4">
-                    {canResetPassword && (
-                        <Link
-                            href={route("password.request")}
-                            className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                        >
-                            Forgot your password?
-                        </Link>
-                    )}
-                    <Link
-                        href={route("register")}
-                        className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ms-2"
+                <div className="flex flex-col gap-2 items-center justify-center mt-4">
+                    <PrimaryButton
+                        className="w-full justify-center"
+                        disabled={processing}
                     >
-                        Haven't registered yet?
-                    </Link>
-                    <PrimaryButton className="ms-2" disabled={processing}>
                         Log in
                     </PrimaryButton>
                 </div>
--- resources/js/Pages/Auth/Register.jsx
+++ resources/js/Pages/Auth/Register.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import GuestLayout from "@/Layouts/GuestLayout";
 import InputError from "@/Components/InputError";
 import InputLabel from "@/Components/InputLabel";
@@ -14,6 +14,16 @@ export default function Register() {
         password: "",
         password_confirmation: "",
     });
+    const [visiblePassword, setVisiblePassword] = useState(false);
+    const [visiblePasswordConfirmation, setVisiblePasswordConfirmation] =
+        useState(false);
+
+    const togglePasswordVisibility = () => {
+        setVisiblePassword(!visiblePassword);
+    };
+    const togglePasswordConfirmationVisibility = () => {
+        setVisiblePasswordConfirmation(!visiblePasswordConfirmation);
+    };
 
     useEffect(() => {
         return () => {
@@ -33,8 +43,8 @@ export default function Register() {
 
             <form onSubmit={submit}>
                 <div>
-                    <h1 className="text-3xl font-extrabold tracking-tight text-gray-900">
-                        Create your account
+                    <h1 className="text-5xl font-bold tracking-tight text-gray-900">
+                        Sign Up
                     </h1>
                 </div>
                 <div className="mt-4">
@@ -86,12 +96,12 @@ export default function Register() {
                     <InputError message={errors.email} className="mt-2" />
                 </div>
 
-                <div className="mt-4">
+                <div className="relative mt-4">
                     <InputLabel htmlFor="password" value="Password" />
 
                     <TextInput
                         id="password"
-                        type="password"
+                        type={visiblePassword ? "text" : "password"}
                         name="password"
                         value={data.password}
                         className="mt-1 block w-full"
@@ -99,11 +109,52 @@ export default function Register() {
                         onChange={(e) => setData("password", e.target.value)}
                         required
                     />
+                    <div
+                        className="absolute inset-y-[3rem] right-0 flex items-center px-4 text-gray-600"
+                        onClick={togglePasswordVisibility}
+                    >
+                        {!visiblePassword ? (
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth={1.5}
+                                stroke="currentColor"
+                                className="w-6 h-6 cursor-pointer"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
+                                />
+                            </svg>
+                        ) : (
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth={1.5}
+                                stroke="currentColor"
+                                className="w-6 h-6 cursor-pointer"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+                                />
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                                />
+                            </svg>
+                        )}
+                    </div>
 
                     <InputError message={errors.password} className="mt-2" />
                 </div>
 
-                <div className="mt-4">
+                <div className="relative mt-4">
                     <InputLabel
                         htmlFor="password_confirmation"
                         value="Confirm Password"
@@ -111,7 +162,7 @@ export default function Register() {
 
                     <TextInput
                         id="password_confirmation"
-                        type="password"
+                        type={visiblePasswordConfirmation ? "text" : "password"}
                         name="password_confirmation"
                         value={data.password_confirmation}
                         className="mt-1 block w-full"
@@ -121,6 +172,47 @@ export default function Register() {
                         }
                         required
                     />
+                    <div
+                        className="absolute inset-y-[3rem] right-0 flex items-center px-4 text-gray-600"
+                        onClick={togglePasswordConfirmationVisibility}
+                    >
+                        {!visiblePasswordConfirmation ? (
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth={1.5}
+                                stroke="currentColor"
+                                className="w-6 h-6 cursor-pointer"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
+                                />
+                            </svg>
+                        ) : (
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth={1.5}
+                                stroke="currentColor"
+                                className="w-6 h-6 cursor-pointer"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+                                />
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                                />
+                            </svg>
+                        )}
+                    </div>
 
                     <InputError
                         message={errors.password_confirmation}
@@ -128,16 +220,24 @@ export default function Register() {
                     />
                 </div>
 
-                <div className="flex items-center justify-end mt-4">
-                    <Link
-                        href={route("login")}
-                        className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                <div className="flex flex-col gap-2 items-center justify-center mt-4">
+                    <PrimaryButton
+                        className="w-full justify-center"
+                        disabled={processing}
                     >
-                        Already registered?
-                    </Link>
-                    <PrimaryButton className="ms-4" disabled={processing}>
-                        Register
+                        Create Account
                     </PrimaryButton>
+                    <div className="flex gap-1 items-center justify-center">
+                        <span className="text-sm text-gray-600">
+                            Already have an account?
+                        </span>
+                        <Link
+                            href={route("login")}
+                            className="text-sm text-sky-600 hover:text-sky-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        >
+                            Sign in
+                        </Link>
+                    </div>
                 </div>
             </form>
         </GuestLayout>
