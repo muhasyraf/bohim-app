8415870a8aae149316ede8b685e5ddfed4dc1ea9
muhasyraf <faizkamil39@gmail.com>
2024-03-25 13:45:23 +0700

    feat(layout): fix & refactor navbar footer

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,14 +1,14 @@
-import { Header } from "@/Layouts/Header";
-import { Footer } from "@/Layouts/Footer";
+import Header from "@/Layouts/Header";
+import Footer from "@/Layouts/Footer";
 
-export default function Home() {
+export default function Home({ auth }) {
     return (
         <>
-            <Header />
+            <Header user={auth.user} />
             <div className="container h-screen mt-20">
                 <h1 className="">This is Content</h1>
             </div>
-           <Footer />
+            <Footer />
         </>
     );
 }
